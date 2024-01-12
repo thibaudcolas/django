08f4fa17a08bc8c3fb7fd25319e138b30b490361
Fabian Braun <fsbraun@gmx.de>
2024-01-12 23:14:04 +0100

    Created `SetPAsswordMixin` to import DRY principle - Refs #34429 Co-authored-by: Natalia <124304+nessita@users.noreply.github.com>

--- django/contrib/auth/forms.py
+++ django/contrib/auth/forms.py
@@ -89,10 +89,10 @@ class UsernameField(forms.CharField):
         }
 
 
-class BaseUserCreationForm(forms.ModelForm):
+class SetPasswordMixin(forms.Form):
     """
-    A form that creates a user, with no privileges, from the given username and
-    password.
+    Form mixin that sets a password for a user offering the option to set an
+    unusable password. If mixed into a class which is not a ModelForm the instance
     """
 
     error_messages = {
@@ -111,6 +111,34 @@ class BaseUserCreationForm(forms.ModelForm):
         help_text=_("Enter the same password as before, for verification."),
     )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages["password_mismatch"],
+                code="password_mismatch",
+            )
+        return password2
+
+    def set_password_and_save(self, user, commit=True):
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+            if hasattr(self, "save_m2m"):
+                self.save_m2m()
+        return user
+
+
+class BaseUserCreationForm(SetPasswordMixin, forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+
     class Meta:
         model = User
         fields = ("username",)
@@ -122,16 +150,6 @@ class BaseUserCreationForm(forms.ModelForm):
             self.fields[self._meta.model.USERNAME_FIELD].widget.attrs[
                 "autofocus"
             ] = True
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError(
-                self.error_messages["password_mismatch"],
-                code="password_mismatch",
-            )
-        return password2
 
     def _post_clean(self):
         super()._post_clean()
@@ -146,11 +164,7 @@ class BaseUserCreationForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-            if hasattr(self, "save_m2m"):
-                self.save_m2m()
+        self.set_password_and_save(user, commit=commit)
         return user
 
 
@@ -462,52 +476,21 @@ class PasswordChangeForm(SetPasswordForm):
         return old_password
 
 
-class AdminPasswordChangeForm(forms.Form):
+class AdminPasswordChangeForm(SetPasswordMixin, forms.Form):
     """
     A form used to change the password of a user in the admin interface.
     """
 
-    error_messages = {
-        "password_mismatch": _("The two password fields didn’t match."),
-    }
     required_css_class = "required"
-    password1 = forms.CharField(
-        label=_("Password"),
-        widget=forms.PasswordInput(
-            attrs={"autocomplete": "new-password", "autofocus": True}
-        ),
-        strip=False,
-        help_text=password_validation.password_validators_help_text_html(),
-    )
-    password2 = forms.CharField(
-        label=_("Password (again)"),
-        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
-        strip=False,
-        help_text=_("Enter the same password as before, for verification."),
-    )
 
     def __init__(self, user, *args, **kwargs):
         self.user = user
         super().__init__(*args, **kwargs)
 
     def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError(
-                self.error_messages["password_mismatch"],
-                code="password_mismatch",
-            )
+        password2 = super().clean_password2()
         password_validation.validate_password(password2, self.user)
         return password2
-
-    def save(self, commit=True):
-        """Save the new password."""
-        password = self.cleaned_data["password1"]
-        self.user.set_password(password)
-        if commit:
-            self.user.save()
-        return self.user
 
     @property
     def changed_data(self):
@@ -516,3 +499,8 @@ class AdminPasswordChangeForm(forms.Form):
             if name not in data:
                 return []
         return ["password"]
+
+    def save(self, commit=True):
+        """Save the new password."""
+        self.set_password_and_save(self.user, commit=commit)
+        return self.user
